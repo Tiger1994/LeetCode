88d3fd5d0097ddee81ed144ffebb2456d2c6bf50
李歇特冯-兹拜因巴哈 <1300288499@qq.com>
2018-06-17 10:06:26 +0800

    Add files via upload

---
+++ Q25.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* reverseKGroup(ListNode* head, int k) {
+		if (k <= 1) return head;
+		ListNode feakHead(0); feakHead.next = head;
+		ListNode *curr; curr = &feakHead;
+
+		ListNode *start = &feakHead;
+		int d_count = 0;
+		while (curr->next){
+			ListNode *rTail, *rHead, *tCurr;
+			rTail = new ListNode(curr->next->val); rHead = rTail;
+			d_count++;
+			int count = 1;
+			curr = curr->next; tCurr = curr;
+
+			while (curr->next && count < k){
+				ListNode* temp = new ListNode(curr->next->val);
+				temp->next = rHead;
+				rHead = temp;
+				curr = curr->next;
+				count++;
+				d_count++;
+			}
+
+			if (count <= k - 1){
+				while (rHead){
+					ListNode *temp = rHead;
+					rHead = rHead->next;
+					delete temp;
+					d_count--;
+				}
+			}
+			else{
+				start->next = rHead;
+				rTail->next = curr->next;
+				start = rTail;
+			}
+		}
+
+		while (head && d_count > 0){
+			ListNode *temp = head;
+			head = head->next;
+			delete temp;
+			d_count--;
+		}
+
+		return feakHead.next;
+	}
+};
+
+int main(void){
+	ListNode *head, *curr;
+	int k = 3;
+	head = new ListNode(1); curr = head;
+	for (int i = 0; i < 4; i++){
+		curr->next = new ListNode(i + 2);
+		curr = curr->next;
+	}
+
+	Solution model;
+	ListNode *result = model.reverseKGroup(head, k);
+
+	return 0;
+}
