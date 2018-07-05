5a851d7995248f5ad61ec029329650b2ff09b8df
李歇特冯-兹拜因巴哈 <1300288499@qq.com>
2018-07-05 19:42:45 +0800

    Add files via upload

---
+++ Q64.cpp
@@ -0,0 +1,29 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+		int m = grid.size(), n = grid[0].size();
+		for (int i = 0; i < m; i++){
+			for (int j = 0; j < n; j++){
+				if (i == 0 && j>0) grid[i][j] += grid[i][j-1];
+				else if (i > 0 && j == 0) grid[i][j] += grid[i - 1][j];
+				else if (i > 0 && j > 0) grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+			}
+		}
+
+		return grid[m - 1][n - 1];
+	}
+};
+
+int main(void){
+	vector<vector<int>> grid = {{1, 3, 1},
+								{1, 5, 1},
+								{4, 2, 1}};
+
+	Solution model;
+	int result = model.minPathSum(grid);
+
+	return 0;}
