e7f406a7c99fb554b3030885ccd565895d71cc9c
李歇特冯-兹拜因巴哈 <1300288499@qq.com>
2019-03-01 13:15:17 +0800

    Update Q41.cpp

--- Q41.cpp
+++ Q41.cpp
@@ -1,29 +1,43 @@
 #include<vector>
+
 using namespace std;
 
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-		if(nums.size() == 0) return 1;
-        vector<bool> records(nums.size(), false);
-		for(int i = 0;i<nums.size();i++){
-			if(nums[i]<=0 || nums[i]>nums.size()) continue;
-			else records[nums[i]-1] = true;
+	int firstMissingPositive(vector<int>& nums) {
+		if (nums.size() == 0) return 1;
+		vector<bool> records(nums.size(), false);
+		for (int i = 0; i<nums.size(); i++) {
+			if (nums[i] <= 0 || nums[i]>nums.size()) continue;
+			else records[nums[i] - 1] = true;
 		}
-		for(int i = 0;i<nums.size();i++){
-			if(records[i]) continue;
-			else return i+1;
+		for (int i = 0; i<nums.size(); i++) {
+			if (records[i]) continue;
+			else return i + 1;
 		}
 
-		return nums.size()+1;
-    }
+		return nums.size() + 1;
+	}
+
+	int firstMissingPositive2(vector<int> &nums) {
+		if (nums.size() == 0) return 1;
+		for (decltype(nums.size()) i = 0; i < nums.size(); i++) {
+			while (nums[i] <= nums.size() && nums[i] > 0 && (nums[i] != nums[nums[i]-1])) {
+				swap(nums[i], nums[nums[i] - 1]);
+			}
+		}
+		for (decltype(nums.size()) i = 0; i < nums.size(); i++) {
+			if (nums[i]!=i+1) return int(i + 1);
+		}
+		return int(nums.size() + 1);
+	}
 };
 
-int main(void){
-	vector<int> nums = {7,8,9,11,12};
+int main(void) {
+	vector<int> nums = { 1,1 };
 
 	Solution model;
-	int result = model.firstMissingPositive(nums);
+	int result = model.firstMissingPositive2(nums);
 
 	return 0;
 }
