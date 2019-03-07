3e6a172de19ca22d2781196fc165f6ed79f8c384
李歇特冯-兹拜因巴哈 <1300288499@qq.com>
2019-03-07 22:46:41 +0800

    Add files via upload

---
+++ Q172.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int trailingZeroes(int n) {
+		int res = 0;
+		while (n) { res += n / 5; n /= 5; }
+		return res;
+	}
+};
+
+int main(void) {
+	int n = 50;
+	Solution mode;
+	int res = mode.trailingZeroes(n);
+	return 0;
+}
